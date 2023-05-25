3875d4f1b972692c2bf98876f364cf0f473f8b63
Eziocdl <eziocdl@gmail.com>
2023-05-25 14:59:23 -0300

    first commit

---
+++ .history/index_20230525102348.js
@@ -0,0 +1,10 @@
+class CadastroUsuario {
+    constructor(nome,sobrenome,login,email,cargo){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.login = login,
+        this.email = email,
+        this.cargo = cargo
+
+   
+}
---
+++ .history/index_20230525104624.js
@@ -0,0 +1,19 @@
+// construindo a classe base do usuario
+class CadastroUsuario {
+    constructor(nome,sobrenome,login,email,cargo){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.login = login,
+        this.email = email,
+        this.cargo = cargo
+    }  
+
+    // retornando as informa
+    exibirInformacoes(){
+        return `Nome: ${this.nome}
+         - Sobrenome: ${this.sobrenome}
+          - Login: ${this.login} 
+          - Email: ${this.email} - Cargo: ${this.cargo}`;
+    }
+
+}
